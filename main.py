9b28c8804d31103c4f0c7056d0fd190190cb12a0
DhvanilSanghvi <sanghvidhvanil@gmail.com>
2020-08-08 11:23:34 +0530

    Main as an app with User integrated

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import hashlib
 import json
 from textwrap import dedent
-from time import time
+from time import sleep
 from uuid import uuid4
 from flask import Flask, jsonify, request
 from urllib.parse import urlparse
@@ -10,32 +10,56 @@ import node1, node2
 import subprocess
 from argparse import ArgumentParser
 import requests
+from User import user
 
 def registerAllNodes(nodes):
-
 	allNodes = {}
 	urls = []
 	for i in range(int(nodes)):
-		urls.append(str(f"http://127.0.0.1:500{i}"))
+		urls.append(f"http://127.0.0.1:500{i}")
 
-	payload = {'nodes' : urls}
+	payload = {"nodes" : urls}
 	print(payload)
 
 	for i in range(int(nodes)):
-		r = request.post(f"http://localhost:500{i}/nodes/register", data = payload)
+		r = requests.post(f"http://localhost:500{i}/nodes/register", json = payload)
+		print(f"Response from server for {i} : {r.text}")
 
-def main():
-	parser = ArgumentParser()
-	parser.add_argument("--nodes")
-	args = parser.parse_args()
-
+def assignNodes(nodes):
+	userNode = {}
 	for i in range(int(args.nodes)+1):
-		if i==0: continue
-		subprocess.Popen(f"python node{i}.py", shell=True)
-		time.sleep(10)
-	registerAllNodes(args.nodes)
+		if i==0: continue 
+		name = input(f"Please enter the name for user at node {i} : ")
+		aadhar = input(f"Please enter Aadhar card number for user at node {i} : ")
+		tempUser = user.User(name, aadhar)
+		userNode[tempUser] = i
+	return userNode
+
+parser = ArgumentParser()
+parser.add_argument("--nodes")
+args = parser.parse_args()
+
+#Starting all the applications
+for i in range(int(args.nodes)+1):
+	if i==0: continue
+	subprocess.Popen(f"python node{i}.py", shell=True)
+	sleep(2)
+
+#Registering all the nodes within each other
+registerAllNodes(args.nodes)
+
+#Dictionary to mantain which user is consuming which node->
+userNode = assignNodes(args.nodes)
+
+
+#Running the app to be consumed by UI
+app = Flask(__name__)
+@app.route('/nodes', methods=['GET'])
+def getAllNodes():
+	pass
+
 
 
 
 if __name__ == "__main__":
-	main()
+	app.run(host='127.0.0.1', port=4990)
